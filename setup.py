1bd21248c8cd33554f4e82e5193f46c050d2097c
Grant Mcconachie <gdmac@scc1.bu.edu>
2026-01-28 15:13:05 -0500

    de-anonymized setup.py and ignored some more batch output files

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name='lora_model',
     description='lora model that combines protein and molecule foudation models.',
-    author='Anonymous',
+    author='Grant D. McConachie',
     packages=find_packages()
 )
